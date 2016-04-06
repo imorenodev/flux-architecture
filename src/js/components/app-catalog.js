b60c733231f4dfa9a3ac6d5e5c05cfc41359eb7f
imorenodev@gmail.com <imorenodev@gmail.com>
2016-04-05 20:19:54 -0400

    working implementation

--- src/js/components/app-catalog.js
+++ src/js/components/app-catalog.js
@@ -2,26 +2,24 @@ import React from 'react';
 import AppStore from '../stores/app-store';
 import CatalogItem from './app-catalogitem';
 
-function getCatalog() {
-  return {
-    items: AppStore.getCatalog()
-  }
+function getCatalog(){
+  return { items: AppStore.getCatalog() }
 }
+
 class Catalog extends React.Component {
-  constructor() {
+  constructor(){
     super();
     this.state = getCatalog()
   }
-  render() {
+  render(){
     let items = this.state.items.map( item => {
-      return <CatalogItem key={item.id} item={item} />
+      return <CatalogItem key={ item.id } item={ item } />
     });
     return (
       <div className="row">
         { items }
       </div>
-    );
+    )
   }
 }
-
 export default Catalog;
--- src/js/components/app-catalogitem.js
+++ src/js/components/app-catalogitem.js
@@ -4,12 +4,17 @@ import CartButton from './app-cart-button';
 
 export default (props) => {
   return (
-    <div className="col-xs-6 col-sm-4 col-md-3">
-      <h4>{props.item.title}</h4>
-      <img src="http://placehold.it/250x250" width="100%" className="img-responsive" />
-      <p>{props.item.summary}</p>
-      <p>{props.item.cost}</p>
-      <CartButton txt="Add To Cart" handler={AppActions.addItem.bind(null, props.item)} />
-    </div>
-  );  
+      <div className="col-xs-6 col-sm-4 col-md-3">
+          <h4>{ props.item.title }</h4>
+          <img src="http://placehold.it/250x250" width="100%" className="img-responsive"/>
+          <p>{ props.item.summary }</p>
+          <p>${ props.item.cost }</p>
+          <CartButton
+            handler={
+              AppActions.addItem.bind(null, props.item)
+            }
+            txt="Add To Cart"
+            />
+      </div>
+  )
 }
